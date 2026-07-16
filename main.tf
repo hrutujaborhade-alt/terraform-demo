2d4359a7b8a7aa53f878100ed337ae56f54c8e67
Hrutuja Borhade <hrutujaborhade@gmail.com>
2026-07-16 11:32:46 +0530

    first commit added

--- main.tf
+++ main.tf
@@ -6,6 +6,9 @@ resource "aws_instance" "demo-instance" {
   ami = "ami-06067086cf86c58e6"
   instance_type = "t3.micro"
 
+tags = {
+  Name = "demo-instance"
+}
 }
 
 # resource "aws_vpc" "demo-vpc" {
